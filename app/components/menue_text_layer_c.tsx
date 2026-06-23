31e15bf4163c6de1ead45241b8bc7ef7c412ca29
mahmoudkhateep7-arch <mahmoudkhateep7@gmail.com>
2026-06-23 03:59:46 +0300

    built soem parts of the menu

--- app/components/menue_text_layer_c.tsx
+++ app/components/menue_text_layer_c.tsx
@@ -1,22 +1,55 @@
+import gsap from "gsap"
 import { allCocktails } from "./dataArraysExported"
+import { useState } from "react"
+import { useGSAP } from "@gsap/react"
+import Top_Text_Menu_C from "./toptextInMenuC"
 
 export default function Menu_text_Layer_c() {
+  const [activId, setActiId] = useState(1)
+  useGSAP(() => {
+
+    gsap.fromTo('.box1', { borderBottomColor: '#a5a0a0', color: '#a5a0a0' }
+      , { color: '#fff', duration: .9, borderBottomColor: '#fff' })
+  }, [])
+  const handleClick = (id: number) => {
+    if (activId != id) {
+
+      gsap.fromTo(`.box${id}`, { borderBottomColor: '#a5a0a0', color: '#a5a0a0' }
+        , { color: '#fff', duration: .9, borderBottomColor: '#fff' })
+
+      gsap.fromTo(`.box${activId}`
+        , { color: '#fff', duration: .9, borderBottomColor: '#fff' }
+
+
+        ,
+        { borderBottomColor: '#a5a0a0', color: '#a5a0a0', onComplete: () => { setActiId(id) } }
+      )
+
+
+    }
+
+  }
+
   return (
     <div className={`relative z-40`}>
       {/* top */}
-      <div className={`grid grid-cols-4 gap-20 px-30 text-xl`}>
-        {allCocktails.map(({ description, id, image, name, title }) => {
-          return (
-            <a
+      <Top_Text_Menu_C handleClick={handleClick}></Top_Text_Menu_C>
 
-              className={`border-b-2 border-white flex justify-center pb-3`}
-              href={'#'} key={id}>
-              {name}
-            </a>
+      {/* flex with justify between */}
+      <div className={`flex justify-between gap-10`}>
+        <a href="#" className={`flex flex-col gap-2`}>
+          <h4>
+            {allCocktails[(activId - 2) == -1 ? 3 : activId - 2].name}
+          </h4>
+        </a>
 
-          )
-        })}
+        <a href="#" className={`flex flex-col gap-2`}>
+          <h4>
+            {allCocktails[(activId % 4)].name}
+          </h4>
+        </a>
       </div>
+
     </div>
   )
 }
---
+++ app/components/toptextInMenuC.tsx
@@ -0,0 +1,20 @@
+import { allCocktails } from "./dataArraysExported"
+
+export default function Top_Text_Menu_C({ handleClick }: { handleClick: (id: number) => void }) {
+  return (
+    <div className={`grid lg:grid-cols-4  gap-20 md:px-4 max-[700px]:hidden grid-cols-2 md:gap-8 lg:px-30 text-xl`}>
+      {allCocktails.map(({ description, id, image, name, title }) => {
+        return (
+          <a
+            onClick={() => { handleClick(id) }}
+
+            className={`border-b-2 box${id} border-[#a5a0a0] text-[#a5a0a0] flex justify-center pb-3`}
+            href={'#'} key={id}>
+            {name}
+          </a>
+
+        )
+      })}
+    </div>
+  )
+}
