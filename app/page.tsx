b3d6ec4886b8aa82265ec9eadae23f67d5268685
mahmoudkhateep7-arch <mahmoudkhateep7@gmail.com>
2026-06-23 08:19:11 +0300

    uncommented all components

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,12 @@
 import { useState } from "react";
 import HeroC from "./components/hero";
 import NavbarC from "./components/navbarC";
-import { allCocktails, cocktailLists, Images, mockTailLists } from "./components/dataArraysExported";
-import Coctail_Right_Leaf_C from "./components/coctail_right_leaf";
-import Coctail_Left_Leaf_C from "./components/coctail_left_leaf";
-import Coctail_Text_Left_C from "./components/coctail_text_left";
-import Coctail_Right_Text_C from "./components/coctail_rigth_text";
 import Coctail_C from "./components/coctail_C";
-import Abotu_Me_Text_C from "./components/aboutmetext";
-import Image from "next/image";
 import BentoImages from "./components/ebntoImages";
 import About_Me_c from "./components/about_me_c";
 import Art_C from "./components/art";
-import Menu_text_Layer_c from "./components/menue_text_layer_c";
+import Menu_section from "./components/menu_section";
+import Contect_c from "./components/contact_c";
 export default function App() {
   const [navHieght, setNavHieght] = useState(60)
 
@@ -25,58 +19,39 @@ export default function App() {
 
 
 
-      {/* <NavbarC setNavHieght={setNavHieght}></NavbarC> */}
-      {/* <HeroC navHieght={navHieght}></HeroC> */}
+      <NavbarC setNavHieght={setNavHieght}></NavbarC>
+      <HeroC navHieght={navHieght}></HeroC>
 
 
       {/* clean up */}
-      {/* <div style={{ height: `calc(100vh - ${navHieght}px)` }} className={``}></div> */}
+      <div style={{ height: `calc(100vh - ${navHieght}px)` }} className={``}></div>
 
 
 
-      {/* <Coctail_C></Coctail_C> */}
+      <Coctail_C navHieght={navHieght}></Coctail_C>
 
       {/* aboutme text section  */}
-      {/* <About_Me_c></About_Me_c> */}
+      <About_Me_c navHieght={navHieght}></About_Me_c>
 
 
-      {/* <BentoImages></BentoImages> */}
+      <BentoImages></BentoImages>
 
 
 
-      {/* <Art_C></Art_C> */}
-      {/* <div className={`h-screen`}></div> */}
+      <Art_C navHieght={navHieght}></Art_C>
+      <div className={`h-screen`}></div>
       <div className={`h-40`}></div>
       {/* menu */}
-      <section className={`py-10 px-2 bg-amber-700`}>
-        <div className={`max-w-7xl mx-auto`}>
-
-          {/* text part upper layer */}
-          <div className={`relative `}>
-
-            {/* all text go in this relative div cuz of lyers */}
-            {/* text component her */}
-            <Menu_text_Layer_c></Menu_text_Layer_c>
-
-
-            {/* absolute leafs later */}
-          </div>
-        </div>
-
-
-      </section>
+      <Menu_section></Menu_section>
 
 
 
 
+      {/* contact */}
+      <Contect_c navHieght={navHieght}></Contect_c>
 
-
-
-      <div className={`h-screen`}></div>
-      <div className={`h-screen`}></div>
-      <div className={`h-screen`}></div>
-      <div className={`h-screen`}></div>
-
+      {/* scroll purpuses */}
+      <div className={`h-20`}></div>
 
 
 
